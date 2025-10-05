3728d2fe42986e5c0a7536dbc89376751ca0dc11
Manuel Lopez <jmlopez.rod@gmail.com>
2025-10-05 14:50:52 -0500

    remove plan subparser

--- pkglink/cli/subparsers/__init__.py
+++ pkglink/cli/subparsers/__init__.py
@@ -2,12 +2,11 @@
 
 import argparse
 
-from . import link, plan, sync, tool
+from . import link, sync, tool
 
 
 def register_all(subparsers: argparse._SubParsersAction) -> None:
     """Register all plk subcommands."""
     link.register(subparsers)
     tool.register(subparsers)
-    plan.register(subparsers)
     sync.register(subparsers)
